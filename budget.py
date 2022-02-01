85bf588b8c81af017cc4f4fbd80abecc168ff4f5
cleon909 <corcoran909@gmail.com>
2022-02-01 21:18:44 +0000

    got the class based methods to work, although not 100% sure why one does work(what does self.__class__ do in an __init__ method?

--- budget.py
+++ budget.py
@@ -1,13 +1,15 @@
-tbal = []
-thist = []
 
 class Budget():
-    
+
+    budgets_history = []
+    budgets = []
+
     def __init__(self, name, balance):
         self.name = name
         self.balance = round(balance,2)
         self.history = []
-        tbal.append(self)
+        self.__class__.budgets.append(self)
+     
 
     def __repr__(self):
         return f"There is £{self.balance} in the {self.name} budget. If you want a print of the history use history method."
@@ -15,13 +17,13 @@ class Budget():
     def deposit(self, amount):
         amount = round(amount,2)
         if amount < 0:
-            print("Negative amount not allowed, use wtihdraw instead")
+            print("Negative amount not allowed, use withdraw instead")
         elif amount == 0:
             print("no change")
         else:
             self.balance += amount
             self.history.append((self.balance, amount))
-            thist.append((self,self.balance,amount))
+            self.budgets_history.append((self.name,self.balance,amount))
             print(f"You have deposited £{amount} into {self.name}. The new balance is £{self.balance}")
             return [round(amount,2), self.balance]
 
@@ -34,7 +36,7 @@ class Budget():
         else:
             self.balance -= amount
             self.history.append((self.balance, -amount))
-            thist.append((self,self.balance,amount))
+            self.budgets_history.append((self.name,self.balance,amount))
             print(f"You have withdrawn £{amount} from {self.name}. The new balance is £{self.balance}")
             return [amount, self.balance]
     
@@ -47,8 +49,8 @@ class Budget():
             budget_to.balance += amount
             self.history.append((self.balance, -amount))
             budget_to.history.append((budget_to.history, amount))
-            thist.append((self,self.balance,-amount))
-            thist.append((budget_to,budget_to.balance,amount))
+            self.budgets_history.append((self.name,self.balance,-amount))
+            self.budgets_history.append((budget_to.name,budget_to.balance,amount))
             print(f"You have transferred £{amount} from {self.name} to {budget_to.name}.\nBalance after transfer:\n\t{self.name}:£{self.balance}\n\t{budget_to.name}:£{budget_to.balance}")
             return [self.balance, amount]
         
@@ -61,8 +63,8 @@ class Budget():
             budget_from.balance -= amount
             self.history.append((self.balance, amount))
             budget_from.history.append((budget_from.history, -amount))
-            thist.append((self,self.balance,amount))
-            thist.append((budget_from,budget_from.balance,-amount))
+            self.budgets_history.append((self.name,self.balance,amount))
+            self.budgets_history.append((budget_from.name,budget_from.balance,-amount))
             print(f"You have transferred £{amount} from {self.name} to {budget_from.name}.\nBalance after transfer:\n\t{self.name}:£{self.balance}\n\t£{budget_from.name}:£{budget_from.balance}")
             return [self.balance, amount]
 
@@ -76,13 +78,22 @@ class Budget():
     def print_balance(self):
         print(f"Your balance of {self.name} is £{self.balance}")
     
-    def print_total_history(total_history):
-        for x in total_history:
-            print(f"Account: {x[0]}, Balance after Transfer: {x[1]}, amount: {x[2]}")
+    def print_total_history(budgets_history=budgets_history):
+        i = 1
+        for x in budgets_history:
+            print(f"{i} - Account: {x[0]}, Balance after Transfer: {x[1]}, amount: £{x[2]}")
+            i += 1
     
-    def print_total_balance(total_balance):
-        bal = sum([x.balance for x in total_balance])
+    def append_budget(self, budgets=budgets):
+        budgets.append(self)
+    
+
+    def print_total_balance(budgets=budgets):
+        bal = sum([x.balance for x in budgets])
         print(f"The balance across all budgets is £{bal}")
+    
+ 
+
     
     
 
--- script.py
+++ script.py
@@ -1,7 +1,5 @@
 from budget import Budget
-
-tbal = []
-thist = []
+budgets = []
 
 # working = True
 
@@ -9,19 +7,18 @@ thist = []
 #     choice = int(input("To create a budget choose 1:\nto add a budget choose 2\nTo withdraw froma budget choose 3:\n to transfer from one budget to another choose 4:\nTo print out an individual budget choose 5:\To print out a history of all budgets choose 6:\nTo exit program choose 9:"))
 
 
-ent = Budget("entertainment", 250)
-# food = Budget("Food", 200)
-# trav = Budget("Travel", 100)
-# booze = Budget("Alcohol", 500)
+ent = Budget("entertainment", 350)
+food = Budget("Food", 200)
+trav = Budget("Travel", 100)
+booze = Budget("Alcohol", 500)
 
 
-# ent.deposit(75.50)
-# food.withdraw(40)
-# food.transfer_from_to(booze,25.25)
-# booze.transfer_to_from(ent,0.73)
-# food.print_history()
-# booze.print_balance()
-# Budget.print_total_history(thist)
-Budget.print_total_balance(tbal)
+ent.deposit(75.50)
+food.withdraw(40)
+food.transfer_from_to(booze,25.25)
+booze.transfer_to_from(ent,0.73)
+food.print_history()
+booze.print_balance()
 
-print(tbal)
+Budget.print_total_history()
+Budget.print_total_balance()
