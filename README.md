8b09c213746bd0b7d3878984ebeba97669a448fe
cleon909 <corcoran909@gmail.com>
2022-02-07 17:59:34 +0000

    added match/case rather than if/else for main menu. now needs python 3.10 to work

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # budget_app
 exercise for oop in python for QA
+
+added match case, now needs python 3.10 to work
--- script.py
+++ script.py
@@ -9,26 +9,26 @@ while working == True:
     choice = ""
     while choice not in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"):
         choice = input("\n\n\nWhat do you want to do?\nInput 1 to create a budget.\nInput 2 to deposit to a budget.\nInput 3 to withdraw from a budget.\nInput 4 to transfer from one budget to another.\nInput 5 to print out the current balance of a budget.\nInput 6 to print out a list of all balances.\nInput 7 to print out an individual budget history.\nInput 8 to print out a history of all budgets.\nInput 9 to print sum of all balances.\nInput 10 to exit program:  ")
-        if choice == "1": #create a budget
-            Budget.create_budget()
-        elif choice == "2": #deposit to budget 
-            Budget.get_input_for_deposit()
-        elif choice == "3": #withdraw from budget
-            Budget.get_input_for_withdraw()
-        elif choice == "4": # transfer between accounts
-            Budget.get_transfer_details()
-        elif choice == "5": # balance of budget
-            Budget.balance_of_budget()
-        elif choice == "6": # print all balances
-            Budget.print_all_balances()
-        elif choice == "7": # history of budget
-            Budget.get_input_for_history()
-        elif choice == "8": # history of all budgets
-            Budget.print_total_history()
-        elif choice =="9": #add up balance of all budgets
-            Budget.print_total_balance()
-        elif choice == "10": # kill program and save state
-            Budget.save_state()
-            working = False
-            print("Goodbye!")
+        match choice:
+            case "1" : Budget.create_budget()
+            case "2" : Budget.get_input_for_deposit()
+            case "3" : Budget.get_input_for_withdraw()
+            case "4" : Budget.get_transfer_details()
+            case "5" : Budget.balance_of_budget()
+            case "6" : Budget.print_all_balances()
+            case "7" : Budget.get_input_for_history()
+            case "8" : Budget.print_total_history()
+            case "9" : Budget.print_total_balance()
+            case "10" : 
+                Budget.save_state()
+                working = False
+                print("Goodbye!")
 
+        
+        
+        
+        
+        
+        
+        
+        
